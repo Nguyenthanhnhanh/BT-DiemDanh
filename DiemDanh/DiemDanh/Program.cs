062805283e27f3a1bd1fbdbe5018692499558970
Nguyenthanhnhanh <80702222+Nguyenthanhnhanh@users.noreply.github.com>
2022-04-26 08:24:13 +0700

    Update Program.cs

--- DiemDanh/DiemDanh/Program.cs
+++ DiemDanh/DiemDanh/Program.cs
@@ -9,29 +9,29 @@ namespace DiemDanh
         }
         class PhuongtrinhBac2
         {
-            private double a;
-            private double b;
-            private double c;
-            public PhuongtrinhBac2(double a, double b, double c)
+            private double a139;
+            private double b139;
+            private double c139;
+            public PhuongtrinhBac2(double a139, double b139, double c139)
             {
-                this.a = a;
-                this.b = b;
-                this.c = c;
+                this.a139 = a139;
+                this.b139 = b139;
+                this.c139 = c139;
             }
-            public double A{ get => a; set => a = value; }
-            public double B { get => b; set =>a = value; }
-            public double C { get => c; set => c = value; }
-            public double delta(double a, double b, double c)
+            public double A139{ get => a139; set => a139 = value; }
+            public double B139 { get => b139; set =>a139 = value; }
+            public double C139 { get => c139; set => c139 = value; }
+            public double delta(double a139, double b139, double c139)
             {
-                return b * b - 4 * c * a;
+                return b139 * b139 - 4 * c139 * a139;
             }
-            public double Nghiem(double a , double b , double c )
+            public double Nghiem(double a139 , double b139 , double c139 )
             {
-                double x1,x2;
-                if (delta(a, b, c) < 0)
+                double x1139,x2139;
+                if (delta(a139, b139, c139) < 0)
                     return -1;
                 else
-                    if (delta(a, b, c) == 0)
+                    if (delta(a139, b139, c139) == 0)
                     return 0;
                 else
                     return 1;
