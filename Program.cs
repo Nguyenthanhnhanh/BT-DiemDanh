98afe2033e71ad75858949bc82bcb8ab0b91b37a
Nguyenthanhnhanh <80702222+Nguyenthanhnhanh@users.noreply.github.com>
2022-04-26 07:58:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Animal
+    {
+        public void eat()
+        { }
+        public void makeSound()
+        { }
+
+    }
+    class Cat: Animal
+    {
+        public void run()
+        { }
+    }
+    class Bird: Animal
+    {
+        public void fly()
+        { }
+    }
+}
