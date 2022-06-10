f3023e0d15f13aa4981415cd8c2a867adf37aab6
Nguyenthanhnhanh <80702222+Nguyenthanhnhanh@users.noreply.github.com>
2022-06-10 10:20:22 +0700

    Add files via upload

---
+++ BTCB2_B33/BTCB2_B33/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCB2_B33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int demcs139;
+            double tong139;
+            Console.WriteLine("So ArmsTrong co 3,4 Chu so: ");
+            for (int i139 = 100; i139 <= 9999; i139++)
+            {
+                tong139 = 0;
+                if (i139 <= 999)
+                {
+                    demcs139 = 3;
+                }
+                else
+                {
+                    demcs139 = 4;
+                }
+                int t139 = i139;
+                while (t139 != 0)
+                {
+                    int x139 = t139 % 10;
+                    tong139 += Math.Pow(x139, demcs139);
+                    t139 = t139 / 10;
+                }
+                if (tong139 == i139)
+                {
+                    Console.Write(i139 + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
