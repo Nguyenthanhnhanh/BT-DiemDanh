72ba344053579c05cbe5859ac1d140549d087d80
Nguyenthanhnhanh <80702222+Nguyenthanhnhanh@users.noreply.github.com>
2022-06-10 10:12:54 +0700

    Add files via upload

---
+++ BTCB2_B13/BTCB2_B13/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCB2_B13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d139, m139, y139, top, dayofweek;
+            Console.Write("Nhap vao ngay = ");
+            d139 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap vao thang = ");
+            m139 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap vao nam = ");
+            y139 = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                if (d139 < 1 || d139 > 31)
+                {
+                    Console.WriteLine("Ngay ko hop le");
+                    break;
+                }
+                if (m139 < 1 || m139 > 12)
+                {
+                    Console.WriteLine("Thang ko hop le");
+                    break;
+                }
+                if (y139 < 1582)
+                {
+                    Console.WriteLine("Nam ko hop le");
+                    break;
+                }
+                switch (m139)
+                {
+                    case 4:
+                        {
+                            top = 30;
+                            break;
+                        }
+                    case 6:
+                        {
+                            top = 30;
+                            break;
+                        }
+                    case 9:
+                        {
+                            top = 30;
+                            break;
+                        }
+                    case 11:
+                        {
+                            top = 30;
+                            break;
+                        }
+                    case 2:
+                        {
+                            if ((y139 % 4 == 0 && y139 % 100 != 0) || (y139 % 400 == 0))
+                            {
+                                top = 29;
+                                break;
+                            }
+                            else
+                            {
+                                top = 28;
+                                break;
+                            }
+
+                        }
+                    default:
+                        {
+                            top = 31;
+                            break;
+                        }
+                }
+                Console.WriteLine("Hop le.");
+                y139 -= (14 - m139) / 12;
+                m139 += 12 * ((14 - m139) / 12) - 2;
+                dayofweek = (d139 + y139 + y139 / 4 - y139 / 100 + y139 / 400 + (31 * m139) / 12) % 7;
+                if (dayofweek == 0)
+                {
+                    Console.WriteLine("Chu nhat");
+                    break;
+                }
+                else
+                {
+                    dayofweek++;
+                    Console.WriteLine("Thu " + dayofweek);
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ BTCB2_B21/BTCB2_B21/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCB2_B21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dc139, d1139, d2139, d3139, sum;
+            char kv139, dt139;
+            Console.Write("Nhap diem chuan: ");
+            dc139 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap diem 3 mon thi: ");
+            Console.Write("Mon 1: ");
+            d1139 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Mon 2:");
+            d2139 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Mon 3: ");
+            d3139 = Convert.ToDouble(Console.ReadLine());
+            if (d1139 * d2139 * d3139 != 0)
+            {
+                Console.Write("Nhap vao khu vuc (A,B,C,X): ");
+                kv139 = Convert.ToChar(Console.ReadLine());
+                Console.Write("Nhap vao doi tuong(1,2,3,0): ");
+                dt139 = Convert.ToChar(Console.ReadLine());
+                sum = d1139 + d2139 + d3139;
+                switch (kv139)
+                {
+                    case 'A':
+                        {
+                            sum += 2;
+                            break;
+                        }
+                    case 'B':
+                        {
+                            sum += 1;
+                            break;
+                        }
+                    case 'C':
+                        {
+                            sum += 0.5;
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+
+                switch (dt139)
+                {
+                    case '1':
+                        {
+                            sum += 2.5;
+                            break;
+                        }
+                    case '2':
+                        {
+                            sum += 1.5;
+                            break;
+                        }
+                    case '3':
+                        {
+                            sum += 1;
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+                if (sum >= dc139)
+                {
+                    Console.WriteLine("Dau - " + sum);
+                }
+                else
+                {
+                    Console.WriteLine("Rot - " + sum);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Rot - " + (d1139 + d2139 + d3139));
+            }
+            Console.ReadKey();
+        }
+    }
+}
