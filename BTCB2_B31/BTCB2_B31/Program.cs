f781ffc268ce9e941c6dd130f461286fe649803b
Nguyenthanhnhanh <80702222+Nguyenthanhnhanh@users.noreply.github.com>
2022-06-10 10:19:21 +0700

    Add files via upload

---
+++ BTCB2_B31/BTCB2_B31/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCB2_B31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bang Cuu Chuong: ");
+            for (int i139 = 1; i139 <= 10; i139++)
+            {
+                for (int j139 = 2; j139 <= 9; j139++)
+                {
+                    Console.Write("{0} x {1} = {2}  ---    ", j139, i139, j139 * i139);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
